0a47b7745887c5e93e4d9c48161b71ec6b05b07d
DrDragren <DrDragren@users.noreply.github.com>
2026-06-13 18:58:59 +0800

    [FIX] Floating footer when there is no content
    - Maximizes the height of the website by maximizing its root height and adding a minimum height for the body (style.css)
    - Added the viewport meta tag for responsive design (index.html, about-me.html, blogs.html, games.html, projects.html, recipes.html)
    - Fixed all the links that need to be fixed
    - All links under the social class now automatically open in a new tab (cookies.js)
    
    [TASK]
    - Make a carousel that wraps around itself (<any>.html, style.css)
    - Animate the scrolling of the carousel to slow down instead of immediate (style.css)
    - Replace the Recipes page with a CV page instead
    - Separate the target attribute adder from cookies.js to another javascript file
    - Resolve LinkedIn Badge Issue
    
    [ISSUE]
    - The LinkedIn Badge disappears randomly. The reason is currently unknown but it is hypothesized that that is just how it is.
         - The external javascript file has been removed as a test but the badge still disappears regardless
         - Reloading the page has also been tried but sometimes it does not work
         - Waiting sometimes works too
         - Searching through the internet, a paid solution has been found but it only has a similar problem. The badge they fix does not match the ones LinkedIn provides.
         - Try checking the LinkedIn Badge JavaScript link and check what exactly it replaces the content of the div with. Copy that and apply it directly to the page instead.
    - @view-transition is not working on my device

--- cookies.js
+++ cookies.js
@@ -33,3 +33,12 @@ document.addEventListener("DOMContentLoaded", () => {
         checkBox.addEventListener("change", () => saveThemeState(checkBox));
     }
 });
+
+//Automatically adds the target="_blank" attribute to all links under the socials class
+
+const socials = document.querySelectorAll(".socials .link");
+if (socials.length > 0) {
+    socials.forEach(link => {
+        link.setAttribute("target", "_blank");
+    });
+}
